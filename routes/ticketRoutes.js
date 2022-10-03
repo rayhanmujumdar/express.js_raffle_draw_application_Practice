cc5cf805b6bc9a20a555bcabbddff42342c0cd97
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2022-10-03 14:34:21 -0500

    ticketRouter all route created

--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -1,18 +1,53 @@
 const status = require('express').Router()
+const myDb = require('../db/myDb')
 
 status.route('/t/:ticketId')
-    .get(() => {})
+    .get((req,res) => {
+        const {ticketId} = req.params
+        const ticket = myDb.findById(ticketId)
+        res.status(200).json(ticket)
+    })
+    .patch((req,res) => {
+        const {ticketId} = req.params
+        const updateTicket = myDb.updateById(ticketId)
+        res.status(201).json({
+            message: 'update successful',
+            updateTicket
+        })
+    })
+    .delete(() => {
+        const {ticketId} = req.params
+        const delTicket = myDb.deleteById(ticketId)
+        res.status(201).json(delTicket)
+    })
+
+status.route('/u/:username')
+    .get((req,res) => {
+        const {username} = req.params
+        const ticket = myDb.findByUser(username)
+        res.status(200).json(ticket)
+    })
     .patch(() => {})
     .delete(() => {})
 
-status.route('/t/:username')
-    .get(() => {})
-    .patch(() => {})
-    .delete(() => {})
-
-status.post('/sell',() => {})
-status.post('/bulk',() => {})
-status.get('/draw',() => {})
-status.get('',() => {})
+status.post('/sell',(req,res) => {
+    const {username,price} = req.body
+    const createTicket = myDb.create(username,price)
+    res.status(201).json(createTicket)
+})
+status.post('/bulk',(req,res) => {
+    const {username,price,quantity} = req.body
+    const bulkTicket = myDb.bulkCreate(username,price,quantity)
+    res.status(201).json(bulkTicket)
+})
+status.get('/draw',(req,res) => {
+    const winnerCount = req.query.wc
+    const winner = myDb.draw(winnerCount)
+    res.status(200).json(winner)
+})
+status.get('',(req,res) => {
+    const tickets = myDb.find()
+    res.status(200).json(tickets)
+})
 
 module.exports = status
